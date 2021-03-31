f12b5fc0b3628faac1ebee464ba6b1e506ded834
Byeori-Kim <99kbr11@gmail.com>
2021-03-31 19:30:41 +0900

    solve question number 14 and 17

---
+++ 17.py
@@ -0,0 +1,27 @@
+
+import math
+def f(x):
+    return math.pow(x,3)-x-1
+a=1.0
+b=2.0
+p=(a+b)/2
+TOL=math.pow(10,-4)
+N0=20
+n=0
+FA=f(a)
+FP=f(p)
+while n<N0:
+    while FP!=0 and (b-a)/abs(a)>=TOL:
+        print(p)
+        if FA*FP>0:
+            a=p
+            FA=FP
+        else:
+            b=p
+        p=(a+b)/2
+        FP=f(p)
+    n=n+1
+    p=(a+b)/2
+print("Answer is",p)
+    
+
